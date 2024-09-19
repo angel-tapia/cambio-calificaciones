899b1628e1df856f52d055f2bba763da5ca43886
Angel Tapia <angeltapia1805@gmail.com>
2024-09-19 14:03:43 -0600

    Update @types/react to version 18.3.8

--- src/hooks/getAlumnos.ts
+++ src/hooks/getAlumnos.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { MateriaAlumnos } from 'src/models';
 
-const BASE_URL = 'http://127.0.0.1:8000';
+const BASE_URL = 'https://backend-calificaciones.vercel.app';
 const ENDPOINT = '/alumnos';
 
 export function getAlumnos(plan: string, subjectId: string, group: string) {
--- src/hooks/getMaterias.ts
+++ src/hooks/getMaterias.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { Profesor } from 'src/models';
 
-const BASE_URL = 'http://127.0.0.1:8000';
+const BASE_URL = 'https://backend-calificaciones.vercel.app';
 const ENDPOINT = '/materias';
 
 export function getMaterias(employeeId: string) {
